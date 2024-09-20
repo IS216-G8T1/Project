b4e2ed6a9991c580027d0ddb2f509208d50440d6
Risheetha <risheethas.2021@scis.smu.edu.sg>
2024-09-20 13:22:50 +0800

    Initial commit
    Created vue project and added routing to about, login, signup and profile pages

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Profile from '../views/Profile.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/About.vue')
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: Profile
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Signup.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/Signup.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>This is a signup page</h1>
+  </div>
+</template>
